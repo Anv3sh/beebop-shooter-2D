697b4fb54d3e54047dcee2f1cf823aa1553c8e72
Anvesh Mishra <anveshmishra0508@gmail.com>
2025-03-30 22:57:09 +0530

    [FEAT] Game over and Restart

--- internals/utils.go
+++ internals/utils.go
@@ -6,3 +6,10 @@ func isColliding(x1, y1, w1, h1, x2, y2, w2, h2 float64) bool {
 		y1 < y2+h2 &&
 		y1+h1 > y2
 }
+
+func getTextWidthAndHeight(text string) (int,int){
+	textWidth := len(text) * 7 // approx. 7 pixels per character
+	textHeight := 13
+
+	return textWidth, textHeight
+}
