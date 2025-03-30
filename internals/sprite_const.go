4e52d8101c393fe3b4ec1eb0440f66cf615c1f23
Anvesh Mishra <anveshmishra0508@gmail.com>
2025-03-30 16:21:32 +0530

    [FEAT] Laser and meteor collision

--- internals/sprite_const.go
+++ internals/sprite_const.go
@@ -7,4 +7,5 @@ const(
 	SPACE_BACKGROUND_PURPLE = "assets/kenney_redux/Backgrounds/purple.png"
 	SPACE_BACKGROUND_BLUE = "assets/kenney_redux/Backgrounds/blue.png"
 	LASER_BLUE_16 = "assets/kenney_redux/PNG/Lasers/laserBlue16.png"
+	METEOR_MED = "assets/kenney_redux/PNG/Meteors/meteorBrown_med1.png"
 )
---
+++ internals/utils.go
@@ -0,0 +1,8 @@
+package internals
+
+func isColliding(x1, y1, w1, h1, x2, y2, w2, h2 float64) bool {
+	return x1 < x2+w2 &&
+		x1+w1 > x2 &&
+		y1 < y2+h2 &&
+		y1+h1 > y2
+}
